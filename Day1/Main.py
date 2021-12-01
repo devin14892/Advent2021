a5f12e9ff7232759a36afce649408cdb4638f0f5
DevinM <Devin14892@hotmail.comM>
2021-12-01 17:22:43 -0500

    Trying to read the file

---
+++ Day1/Main.py
@@ -0,0 +1,12 @@
+def read_input(filename):
+    with open(filename, 'r') as fd:
+        parsed = list(fd.read())
+    return parsed
+
+def main():
+    depths = read_input('input')
+    print (depths)
+
+
+if __name__ == '__main__':
+    main()
